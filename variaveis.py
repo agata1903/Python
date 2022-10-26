1d35cc6a389669edb245372e7d51958db1a22f39
Ágata <59833071+agata1913@users.noreply.github.com>
2022-10-26 10:32:34 -0300

    Add files via upload

---
+++ variaveis.py
@@ -0,0 +1,9 @@
+john = 3
+mary = 5
+adam = 6
+soma = john + mary + adam
+
+print("John possui ",john, "maçãs", end=", ")
+print("Mary possui ",mary, "maçãs ", end="e ")
+print("Adam possui ",adam, "maçãs", end=". ")
+print("Juntos, os três possuem ",soma, "maçãs", end=".")
