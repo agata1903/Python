8bbea035f33d823561df0a91fb7fd4aa4c718c82
Ágata <59833071+agata1903@users.noreply.github.com>
2022-12-07 09:28:02 -0300

    Add files via upload

--- sen_cos_tan.py
+++ sen_cos_tan.py
@@ -1,8 +1,8 @@
-from math import cos,sin,tan
+import math
 num = int(input('Digite um número: '))
-seno = sin(num)
-cosseno = cos(num)
-tangente = tan(num)
+seno = math.sin(math.radians(num))
+cosseno = math.cos(math.radians(num))
+tangente = math.tan(math.radians(num))
 
 print('O seno de ',num, ' é ', seno, end='.\n')
 print('O cosseno de ',num, ' é ', seno, end='.\n')
