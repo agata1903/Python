54f9ffa9d82aea1fa1c20e6412147653852d3552
Ágata <59833071+agata1903@users.noreply.github.com>
2023-01-05 11:33:57 -0300

    Delete aluguel_carros.py

--- aluguel_carros.py
+++
@@ -1,7 +0,0 @@
-dias = int(input('Digite a quantidade de dias alugados: '))
-custo_dia = dias * 60
-km = float(input('Digite a quantidade de km rodados: '))
-custo_km = km * 0.15
-
-custo_total = custo_dia + custo_km
-print('Você rodou por ', dias, 'dias e ', km, 'km. Você deverá pagar R$', custo_total)
