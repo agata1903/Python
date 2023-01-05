38a4294433e2e41193e0cb9a208fe032bd51b0e3
Ágata <59833071+agata1903@users.noreply.github.com>
2023-01-05 11:42:23 -0300

    Delete hipotenusa.py

--- hipotenusa.py
+++
@@ -1,5 +0,0 @@
-import math
-cat1 = float(input('Insira o valor do cateto oposto: '))
-cat2 = float(input('Insira o valor do cateto adjacente: '))
-hipo = math.hypot(cat1,cat2)
-print('O valor da hipotenusa é: ', hipo)
