7b0e606ded8ef395ea48ee7ecbf14c793a289aa9
Ágata <59833071+agata1903@users.noreply.github.com>
2023-01-05 11:41:54 -0300

    Delete conversao_dolares.py

--- conversao_dolares.py
+++
@@ -1,3 +0,0 @@
-valor_real = float(input('Digite o valor em real: '))
-valor_convertido = round(valor_real / 5.35, 2)
-valor_final = print('Com ', valor_real, 'reais, você pode conseguir', valor_convertido, 'dólares')
