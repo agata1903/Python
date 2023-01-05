3145b02880538ec2ba29a74d837985e528625e42
Ágata <59833071+agata1903@users.noreply.github.com>
2023-01-05 11:26:03 -0300

    Delete adivinhacao.py

--- adivinhacao.py
+++
@@ -1,9 +0,0 @@
-import random
-
-sorteio = random.randint(0, 5)
-num = int(input('Escolha um número entre 0 e 5:'))
-
-if num == sorteio:
-    print('Parabéns, eu pensei no número', sorteio,'! Você sabe ler mentes!')
-else:
-    print('Ahhh, não foi dessa vez! Eu pensei no número', sorteio)
