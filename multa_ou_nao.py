8137954e26b81a1a165f9c3a498be5a986db51b4
Ágata <59833071+agata1903@users.noreply.github.com>
2023-01-09 12:50:52 -0300

    Delete multa_ou_nao.py

--- multa_ou_nao.py
+++
@@ -1,8 +0,0 @@
-km = int(input('Digite a quilometragem: '))
-dif = km - 80
-multa = 7*dif
-
-if km > 80:
-    print('Seu danadinho, desrespeitando as leis de trânsito, né? Sua multa será de R$', multa, end=',00')
-else:
-    print('Pode passar, meu jovem, tenha uma boa viagem!')
