0746631a4710ba5e80dee7348a819cb8a0e470d0
Ágata <59833071+agata1903@users.noreply.github.com>
2023-01-09 12:52:11 -0300

    Delete sen_cos_tan.py

--- sen_cos_tan.py
+++
@@ -1,9 +0,0 @@
-import math
-num = int(input('Digite um número: '))
-seno = math.sin(math.radians(num))
-cosseno = math.cos(math.radians(num))
-tangente = math.tan(math.radians(num))
-
-print('O seno de ',num, ' é ', seno, end='.\n')
-print('O cosseno de ',num, ' é ', seno, end='.\n')
-print('A tangente de ',num, ' é ', seno, end='.')
